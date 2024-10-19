280b77293b57f4f96679e568b34db1ea42f71628
Mitu Adrian <adrianmitu83@gmail.com>
2024-10-19 13:00:06 +0300

    Removed intial project and made clone project the new main one

--- LibraryManager/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module gui.librarymanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.xml;
-
-
-    opens gui.librarymanager to javafx.fxml;
-    exports gui.librarymanager;
-}
